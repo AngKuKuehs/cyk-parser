b1bfa6d9056a1b5f27cb5cf008a96665e83e77e4
AngKuKuehs <bryananghe.dev@gmail.com>
2024-07-08 22:22:07 +0800

    Updated for new grammar format.
    Ammended load_grammar_from_json function. Removed boilerplate/duplication. Functionalized code. Added print statements and debug variable.

--- CYK_parser.py
+++ CYK_parser.py
@@ -1,83 +1,98 @@
 import json
 
-def load_grammar_from_json(path: str) -> dict:
+from Item import Item
+
+debug = False
+
+def load_grammar_from_json(path: str) -> list:
+    print(f"Loading {path}") if debug else ""
     with open(path) as f:
        productions = (json.load(f))
-    result = []
+    print(f"\tproductions: {productions}") if debug else ""
+    grammar = []
     for key, values in productions.items():
         for value in values:
-            result.append((key, value))
-    # print(result)
-    return result
+            if type(value) == dict:
+                for name, result in value.items():
+                    print(f"name: {name}, result: {result}") if debug else ""
+                    item = Item(start=key, result=tuple(result), state=0, error_metric=0, next_symbol=result[0] if result else (), name=name)
+                    grammar.append(item)
+            else:
+                item = Item(start=key, result=tuple(value), state=0, error_metric=0, next_symbol=value[0] if value else ())
+                grammar.append(item)
+    print(f"\tGrammar: {grammar}") if debug else ""
+    return grammar
 
-def parse(sentence: str, grammar: dict) -> bool:
-    # print(f"Sentence: {sentence}")
-    n = len(sentence)
-    item_chart = [[[] for _ in range(n)] for _ in range(n)]
-    symbol_chart = [[{} for _ in range(n)] for _ in range(n)]
-    # print(f"item_chart: {item_chart}")
-    # print(f"symbol_chart: {symbol_chart}")
-    for i in range(n):
-        symbol_chart[i][i][sentence[i]] = 0
-        # print(f"current i: {i}")
-        add_to_symbols = True
-        while add_to_symbols:
-            add_to_symbols = False
-            for rule in grammar:
-                # print(f"rule: {rule}")
-                if rule[1][0] in symbol_chart[i][i]:
-                    # print(f"\tCan progress")
-                    if len(rule[1]) == 1  and rule[0] not in symbol_chart[i][i]:
-                        # print(f"\t\tadding to symbol chart: {rule[0], 0}")
-                        symbol_chart[i][i][rule[0]] = 0
-                        add_to_symbols = True
-                    elif (rule[0], rule[1], 1, 0) not in item_chart[i][i] and len(rule[1]) != 1:
-                        item = (rule[0], rule[1], 1, 0)
-                        # print(f"\t\titem to append to item_chart at {i, i}")
-                        # print(f"\t\t\titem: {item}")
-                        item_chart[i][i].append(item)
-                        # print(f"\t\tupdated item_chart: {item_chart}")
+def saturate(row: int, col: int, grammar: list, symbol_chart: list, item_chart: list) -> None:
+    add_to_symbols = True
+    while add_to_symbols:
+        add_to_symbols = False
+        for item in grammar:
+            print(f"\tItem: {item}") if debug else ""
+            symbol_cell = symbol_chart[row][col]
+            if item.next_symbol in symbol_cell:
+                if item.completes() and item.start not in symbol_cell:
+                    print(f"\t\tItem Completes, adding {item.start} to symbol chart") if debug else ""
+                    symbol_chart[row][col][item.start] = 0
+                    add_to_symbols = True
+                    print(f"\t\t\tSymbol Chart: {symbol_chart}") if debug else ""
+                elif item.signature not in item_chart[row][col] and not item.completes():
+                    new_item = Item(item.start, item.result, item.state + 1, item.error_metric, item.result[item.state + 1], item.name)
+                    print(f"\t\tItem progressing, adding {new_item} to item chart") if debug else ""
+                    item_chart[row][col][new_item.signature] = new_item
+                    print(f"\t\t\tItem Chart: {item_chart}") if debug else ""
 
-    for skip in range(1, n):
-        # print(f"item_chart: {item_chart}")
-        # print(f"symbol_chart: {symbol_chart}")
-        # print(f"skip: {skip}")
-        for row in range(0, n - skip):
-            # print(f"\trow: {row}")
-            curr_cell = (row, row + skip)
-            # print(f"\tcurr_cell: {curr_cell}")
-            for i in range(1, skip + 1):
-                # print(f"\t\ti: {i}")
-                prev_item_cell = (curr_cell[0], curr_cell[1] - i)
-                prev_symbol_cell = (curr_cell[0] + (skip + 1 - i), curr_cell[1])
-                # print(f"\t\tprev_item_cell: {prev_item_cell}")
-                # print(f"\t\tprev_symbol_cell: {prev_symbol_cell}")
-                for item in item_chart[prev_item_cell[0]][prev_item_cell[1]]:
-                    # print(f"\t\titem: {item}")
-                    progress_char = item[1][item[2]]
-                    # print(f"\t\t\tprogress_char: {progress_char}")
-                    prev_symbols = symbol_chart[prev_symbol_cell[0]][prev_symbol_cell[1]]
-                    if progress_char in prev_symbols:
-                        # print(f"\t\t\t\t Progress char found")
-                        if item[2] + 1 == len(item[1]):
-                            # # print(f"\t\t\t\t\titem can be completed! add to prev_symbols")
-                            symbol_chart[curr_cell[0]][curr_cell[1]][item[0]] = item[3] + prev_symbols[progress_char]
-                            add_to_symbols = True
+def fill_diagonal(n: int, sentence: str, grammar: list, symbol_chart: list, item_chart: list) -> None:
+    for row in range(n):
+        col = row + 1
+        curr_token = sentence[row]
+        symbol_chart[row][col][curr_token] = 0
+        print(f"Saturating: {(row, col)}") if debug else ""
+        saturate(row, col, grammar, symbol_chart, item_chart)
+
+def fill_rest(n: int, grammar: list, symbol_chart: list, item_chart: list) -> None:
+    for length in range(1, n):
+        print(f"\tlength: {length}") if debug else ""
+        for row in range(0, n - length):
+            col = row + length + 1
+            print(f"\t\trow, col: {row, col}") if debug else ""
+            for split in range(1, length + 1):
+                print(f"\t\t\tsplit: {split}") if debug else ""
+                item_row = row
+                item_col = col - split
+                sym_row = col - split
+                sym_col = col
+                for item in item_chart[item_row][item_col].values():
+                    print(f"\t\t\t\tItem: {item}") if debug else ""
+                    symbol_cell = symbol_chart[sym_row][sym_col]
+                    if item.next_symbol in symbol_cell:
+                        if item.completes(): 
+                            print(f"\t\t\t\tItem Completes, adding {item.start} to symbol chart") if debug else ""
+                            symbol_chart[row][col][item.start] = item.error_metric + symbol_cell[item.next_symbol]
+                            print(f"\t\t\t\tSymbol Chart: {symbol_chart}") if debug else ""
                         else:
-                            new_item = (item[0], item[1], item[2] + 1, item[3])
-                            item_chart[curr_cell[0]][curr_cell[1]].append(new_item)
+                            new_item = Item(item.start, item.result, item.state + 1, item.error_metric, item.result[item.state + 1])
+                            item_chart[row][col][new_item.signature] = new_item
+                            print(f"\t\tItem progressing, adding {new_item} to item chart") if debug else ""
+                            print(f"\t\tItem Chart: {item_chart}") if debug else ""
+                saturate(row, col, grammar, symbol_chart, item_chart)
 
-                while add_to_symbols:
-                    add_to_symbols = False
-                    curr_symbols = symbol_chart[curr_cell[0]][curr_cell[1]]
-                    for rule in grammar:
-                        if rule[1][0] in curr_symbols:
-                            if len(rule[1]) == 1 and rule[0] not in curr_symbols:
-                                symbol_chart[curr_cell[0]][curr_cell[1]][rule[0]] = 0
-                                curr_symbols = symbol_chart[curr_cell[0]][curr_cell[1]]
-                                add_to_symbols = True
-                            elif (rule[0], rule[1], 1, 0) not in item_chart[curr_cell[0]][curr_cell[1]] and len(rule[1]) != 1:
-                                item = (rule[0], rule[1], 1, 0)
-                                item_chart[curr_cell[0]][curr_cell[1]].append(item)
+def parse(sentence: str, grammar: list) -> tuple:
+    print(f"{'*'*30}\nParsing \"{sentence}\" with {grammar}\n{'*'*30}") if debug else ""
+    n = len(sentence)
+    item_chart = [[{} for _ in range(n + 1)] for _ in range(n + 1)]
+    symbol_chart = [[{} for _ in range(n + 1)] for _ in range(n + 1)]
+
+    fill_diagonal(n, sentence, grammar, symbol_chart, item_chart)
+    if debug:
+        print("first diagonal filled")
+        print(f"\titem_chart: {item_chart}")
+        print(f"\tsymbol_chart: {symbol_chart}")
+
+    fill_rest(n, grammar, symbol_chart, item_chart)
+    if debug:
+        print("rest filled")
+        print(f"\titem_chart: {item_chart}")
+        print(f"\tsymbol_chart: {symbol_chart}")
 
     return (symbol_chart, item_chart)
