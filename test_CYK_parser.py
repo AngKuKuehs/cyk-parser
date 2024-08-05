ca7c67ed8ca4f4608809b981daedae3a00a81735
AngKuKuehs <bryananghe.dev@gmail.com>
2024-08-05 21:35:57 +0800

    test: remove incorrect tests

--- test_CYK_parser.py
+++ test_CYK_parser.py
@@ -34,24 +34,24 @@ def test_parser_on_grammar_3():
     assert "Start" in parse("ef", productions_3, init_items_3)[0][0][2]
 
 def test_parser_on_gramamr_4():
-    assert "Start" in parse("", productions_4, init_items_4, debug=False)[0][0][0]
     assert "Start" in parse("()", productions_4, init_items_4, debug=False)[0][0][2]
     assert "Start" in parse("()[]", productions_4, init_items_4, debug=False)[0][0][4]
     assert "Start" in parse("([])", productions_4, init_items_4, debug=False)[0][0][4]
     assert "Start" in parse("[()]", productions_4, init_items_4, debug=False)[0][0][4]
+    assert "Start" in parse("[[[]]]", productions_4, init_items_4, debug=False)[0][0][6]
 
     assert "Start" not in parse("[(])", productions_4, init_items_4, debug=False)[0][0][4]
     assert "Start" not in parse("(])", productions_4, init_items_4, debug=False)[0][0][3]
 
 def test_parser_on_gramamr_5():
-    assert "Start" in parse("", productions_5, init_items_5, debug=False)[0][0][0]
-    assert "Start" in parse("XX", productions_5, init_items_5, debug=False)[0][0][2]
     assert "Start" in parse("X", productions_5, init_items_5, debug=False)[0][0][1]
+    assert "Start" in parse("XX", productions_5, init_items_5, debug=False)[0][0][2]
+    assert "Start" in parse("XXX", productions_5, init_items_5, debug=False)[0][0][3]
     assert "Start" in parse("XXXX", productions_5, init_items_5, debug=False)[0][0][4]
-
-    assert "Start" not in parse("XXX", productions_5, init_items_5, debug=False)[0][0][3]
-    assert "Start" not in parse("XXXXX", productions_5, init_items_5, debug=False)[0][0][3]
+    assert "Start" in parse("XXXXX", productions_5, init_items_5, debug=False)[0][0][5]
 
 test_parser_on_grammar_1()
 test_parser_on_grammar_2()
 test_parser_on_grammar_3()
+test_parser_on_gramamr_4()
+test_parser_on_gramamr_5()
