3fe0e6e2025204d703f888c2793e640058c61559
AngKuKuehs <bryananghe.dev@gmail.com>
2024-12-28 19:06:10 +0800

    refactor: change str to variable

--- examples/gen_small_file_trees.py
+++ examples/gen_small_file_trees.py
@@ -11,28 +11,32 @@ python_productions, init_items_python = load_productions_from_json("grammars/pyt
 
 # generate parse trees for all small python example files
 def generate_small_file_trees():
-    for file in os.listdir("references/example_small_files"):
+    directory = "references/example_small_files"
+
+    for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
         # generate tree from lark
         lark_tree = None
         try:
-            lark_tree = parser.parse(read(f"./references/example_small_files/{file}"))
+            lark_tree = parser.parse(read(file_path))
             lark_tree = convert_lark_tree(lark_tree)
             lark_tree = Tree(lark_tree.data, trim_children(lark_tree.children))
-            save_tree(lark_tree, path=f"./outputs/trees/tmp/{file[:-3]}_tree_lark")
+            save_tree(lark_tree, path=f"./outputs/trees/tmp/{file_name[:-3]}_tree_lark")
         except Exception as e:
-            print(f"lark: {file} failed: {e}")
+            print(f"lark: {file_name} failed: {e}")
 
         # generate tree from own parser
         cyk_tree = None
         try:
-            tokens = parser.lex(read(f"./references/example_small_files/{file}") + "\n")
+            tokens = parser.lex(read(file_path) + "\n")
             symbol_chart, _ = parse(tokens, python_productions, init_items_python)
             cyk_tree = symbol_chart[0][-1]["file_input"][1]
             cyk_tree = Tree(cyk_tree.data, trim_children(cyk_tree.children))
-            save_tree(cyk_tree, path=f"./outputs/trees/tmp/{file[:-3]}_tree")
+            save_tree(cyk_tree, path=f"./outputs/trees/tmp/{file_name[:-3]}_tree")
         except Exception as e:
-            print(f"cyk: {file} failed: {e}")
+            print(f"cyk: {file_name} failed: {e}")
 
+        # ensure trees are equal
         assert(lark_tree == cyk_tree)
 
 if __name__ == "__main__":
