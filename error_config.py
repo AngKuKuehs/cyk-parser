f575c5ebf5487398a53681fd750b5ece9c2e3e35
AngKuKuehs <bryananghe.dev@gmail.com>
2025-03-12 16:30:53 +0800

    chore: add config for tracking insertions

--- error_config.py
+++ error_config.py
@@ -6,12 +6,11 @@ def no_correction_config():
         "error_correct": False,
         "init_error": 0,
         "del_error_combiner": simple_del_addition,
-        "ins_error_combiner": simple_ins_addition,
         "std_error_combiner": simple_std_addition,
         "front_end_del_error_combiner": simple_front_end_del_addition,
         "error_comparator": simple_error_compare,
         "limit_comparator": simple_limit_compare,
-        "init_ins_error": 0,
+        "init_ins_error": 1, # TODO: when this is 0, error correction still happens for some reason
         "init_del_error": 0, # not actually used in error metric calcs since deletions aren't part of the tree
         "error_limit": 0
         }
@@ -30,4 +29,15 @@ def custom_cost_correction_config(error_limit=1, deletion_cost=1, insertion_cost
     config["init_ins_error"] = insertion_cost
     config["init_del_error"] = deletion_cost
     config["error_limit"] = error_limit
+    return config
+
+def track_ins_config(error_limit=(1, 1)):
+    config = custom_cost_correction_config(error_limit=error_limit, deletion_cost=(1, 0), insertion_cost=(1, 1))
+    config["init_error"] = (0, 0)
+    config["del_error_combiner"] = track_ins_del_addition
+    config["std_error_combiner"] = track_ins_std_addition
+    config["front_end_del_error_combiner"] = track_ins_front_end_del_addition
+    config["error_comparator"] = ord_error_compare
+    config["limit_comparator"] = ord_limit_compare
+    config["init_ins_error"] = (1, 1)
     return config
