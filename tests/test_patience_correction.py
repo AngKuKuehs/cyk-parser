757e7a38702f6d59694e2df57c8fe67878884140
AngKuKuehs <bryananghe.dev@gmail.com>
2025-03-22 15:20:21 +0800

    test: add tests for patience correction

--- tests/test_patience_correction.py
+++ tests/test_patience_correction.py
@@ -8,54 +8,51 @@ bracket_productions, bracket_init_items = load_productions_from_json("grammars/s
 # productions_5, init_items_5 = load_productions_from_json("grammars/test.json", debug=False)
 
 # error_config = basic_correction_config(error_limit=5)
-error_config = custom_cost_correction_config(error_limit=2, deletion_cost=1, insertion_cost=1)
+error_config = custom_cost_correction_config(error_limit=2, deletion_cost=2, insertion_cost=1)
 
 def test_patience_correction():
     cyk_tree = patience_parse("()", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
-    assert cyk_tree
+    assert cyk_tree[0]
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/0-{i}_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/0-{i}_tree", text=f"{cyk_tree[i][2]}")
 
-    cyk_tree = patience_parse("(])", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
+    cyk_tree = patience_parse("(]])", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
     assert cyk_tree
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/1-{i}_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/1-{i}_tree", text=f"{cyk_tree[i][2]}")
 
     cyk_tree = patience_parse("[)))]", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/2-{i}_tree")
-
-    cyk_tree = ec_parse("[)))]", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
-    save_tree(cyk_tree, path=f"./outputs/trees/patience_correction_brackets/ec_2_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/2-{i}_tree", text=f"{cyk_tree[i][2]}")
 
     cyk_tree = patience_parse("[))]]", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
     assert cyk_tree
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/3-{i}_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/3-{i}_tree", text=f"{cyk_tree[i][2]}")
 
     cyk_tree = patience_parse("(", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
     assert cyk_tree
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/4-{i}_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/4-{i}_tree", text=f"{cyk_tree[i][2]}")
 
     cyk_tree = patience_parse("", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
     assert cyk_tree
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/5-{i}_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/5-{i}_tree", text=f"{cyk_tree[i][2]}")
 
     cyk_tree = patience_parse(")))()", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
     assert cyk_tree
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/6-{i}_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/6-{i}_tree", text=f"{cyk_tree[i][2]}")
 
     cyk_tree = patience_parse("[()0[", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
     assert cyk_tree
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/7-{i}_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/7-{i}_tree", text=f"{cyk_tree[i][2]}")
 
-    cyk_tree = patience_parse("[()()]", bracket_productions, bracket_init_items, error_config=basic_correction_config(error_limit=5), hard_limit=5, start_token="Start", debug=False)
+    cyk_tree = patience_parse("[()()]", bracket_productions, bracket_init_items, error_config=error_config, hard_limit=5, start_token="Start", debug=False)
     assert cyk_tree
     for i in range(len(cyk_tree)):
-        save_tree(cyk_tree[i], path=f"./outputs/trees/patience_correction_brackets/8-{i}_tree")
+        save_tree(cyk_tree[i][0], path=f"./outputs/trees/patience_correction_brackets/8-{i}_tree", text=f"{cyk_tree[i][2]}")
 
 test_patience_correction()
